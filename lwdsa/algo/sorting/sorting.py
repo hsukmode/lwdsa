5fceef5ed941235c112908cc22c1de97e38530c0
hsuk mode <kushsikand@gmail.com>
2020-11-21 09:56:36 -0800

    addition of sorts, implementation of bubble sort

--- lwdsa/algo/sorting/sorting.py
+++ lwdsa/algo/sorting/sorting.py
@@ -5,18 +5,17 @@ from typing import Sequence, List
 from concurrent.futures import ThreadPoolExecutor
 
 
-
 def sleep_sort(nums: List[int]) -> List[int]:
     """sleep sort algorithm introduced through 4chan
-    time.sleep on input is run in a seperate thread, when thread completes, it is appended to 
+    time.sleep on input is run in a seperate thread, when thread completes, it is appended to
     list.
-    #No guarantees this will return the correct result every time
+    ##No guarantees this will return the correct result every time##
 
     Args:
         nums (List[int]): to swap in place requires a list of integers
 
     Returns:
-        List[int]: potentially sorted list of initial 
+        List[int]: potentially sorted list of nums
     """
     if len(nums) > 10:
         raise Exception("sleep sort will take too long on this input!")
@@ -35,32 +34,38 @@ def sleep_sort(nums: List[int]) -> List[int]:
     return sorted_results
 
 
-
 def bubble_sort(nums: List[int]) -> List[int]:
     """bubble sort algorithm is implemented as follows
     keep a max value
     iterate through the list
 
+    Time Complexity (O(N)^2)
+
     Args:
-        nums (List[int]): [description]
+        nums (List[int]): list of nums
 
     Returns:
-        List[int]: [description]
+        List[int]: sorted list of nums
     """
-    max_val = float('-inf')
-    for idx in range(len(nums) - 1):
-        if nums[idx] > nums[idx +1]:
-            nums[idx + 1], nums[idx] = nums[idx], nums[idx + 1]
-        else:
-            nums[idx] > nums[idx +1]
+    for counter in range(len(nums)):
+        for idx in range(len(nums) - 1 - counter):
+            if nums[idx] > nums[idx + 1]:
+                nums[idx + 1], nums[idx] = nums[idx], nums[idx + 1]
 
-        
-    
     return nums
 
 
 def selection_sort(nums: List[int]) -> List[int]:
-    pass
+    """selection sort algorithm is implemented as follows
+
+    Args:
+        nums (List[int]): list of numbers
+
+    Returns:
+        List[int]: sorted list of nums
+    """
+    
+
 
 def insertion_sort(nums: List[int]) -> List[int]:
     pass
@@ -69,16 +74,22 @@ def insertion_sort(nums: List[int]) -> List[int]:
 def merge_sort(nums: List[int]) -> List[int]:
     pass
 
+
 def quick_sort(nums: List[int]) -> List[int]:
     pass
+
 
 def counting_sort(nums: List[int]) -> List[int]:
     pass
 
+
 def bucket_sort(nums: List[int]) -> List[int]:
     pass
+
 
 def radix_sort(nums: List[int]) -> List[int]:
     pass
 
 
+def heap_sort(nums: List[int]) -> List[int]:
+    pass
---
+++ tests/sorting_test.py
@@ -0,0 +1,4 @@
+from lwdsa.algo.sorting import bubble_sort
+
+def test_bubble_sort():
+    assert bubble_sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
